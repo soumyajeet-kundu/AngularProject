3b825473683af38740aa21db831951647f2f7b83
soumyajeet-kundu <soumyajeet.kundu@ltts.com>
2021-04-26 11:01:31 +0530

    added mysql database

--- registration/src/main/java/com/app/registration/RegistrationApplication.java
+++ registration/src/main/java/com/app/registration/RegistrationApplication.java
@@ -1,9 +1,13 @@
 package com.app.registration;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@EnableAutoConfiguration
+
 public class RegistrationApplication {
 
 	public static void main(String[] args) {
