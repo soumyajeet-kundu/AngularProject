3603c78e33c47b9d1cc5ab744dc676f02ff6822a
soumyajeet-kundu <soumyajeet.kundu@ltts.com>
2021-05-16 20:21:43 +0530

    new component added

--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -23,7 +23,7 @@ export class LoginComponent implements OnInit {
     this._service.loginUserFromRemote(this.user).subscribe(
       data => {
         console.log("response received");
-        this._router.navigate(['/uploaded'])
+        this._router.navigate(['/loginsuccess'])
     },
       error => {
         console.log("error occured");
